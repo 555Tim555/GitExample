a37e31801691d2f2fdc14ff572b2a649d9f8c8ea
Tim <s10114105@gmail.com>
2018-09-24 08:42:27 +0800

    測試看看修改後的 Commit

--- app/src/main/java/com/example/tim/gitexample/MainActivity.java
+++ app/src/main/java/com/example/tim/gitexample/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    int a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
